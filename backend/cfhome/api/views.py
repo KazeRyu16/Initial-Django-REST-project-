bdc258fb579cdf174380cf98e1a121a5e06de2c7
Amir Hussain <fixcruci10@gmail.com>
2026-09-23 21:54:55 +0530

    worked with models manually

--- backend/cfhome/api/views.py
+++ backend/cfhome/api/views.py
@@ -1,23 +1,13 @@
-import json
+import json #imports json module
 from django.http import JsonResponse
+from products.models import product
 
 def api_home(request, *args, **kwargs):
-
-    print(request.GET) #url query params
-    print(request.POST)
-    body = request.body
+    model_data = product.objects.all().order_by("?").first()
     data = {}
-    try:
-        data = json.loads(body)
-    except:
-        pass
-
-    #print(data)
-    data['params'] = dict(request.GET) 
-    data['headers'] = dict(request.headers) # dict --> means store the data in the data dictionary
-    print(data['headers'])
-    # print(data['headers'])    #prints evrything content_type,length,etc included
-    # print(request.content_type) #prints the content type
-    return JsonResponse(data) # returns json data
-
-
+    if model_data :
+        data['id'] = model_data.id
+        data['title'] = model_data.title
+        data['content'] = model_data.content
+        data['price'] = model_data.price
+    return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 # endpoint = "https://httpbin.org/status/200/"
 # endpoint = "https://httpbin.org/anything"
@@ -8,4 +9,3 @@ get_responce = requests.get(endpoint, params={"abc":123}, json={"query": "Hello 
 # print(get_responce.text) prints raw text response (which means the source code of the endpoint)
 print(get_responce.status_code) # prints the status code (200 means working successfully and so on )
 print(get_responce.json()) # prints the json response ( json refers to javascript object notation)
-
